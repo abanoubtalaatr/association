ebd838a5f86c7d149cc932feb1e1d21b16965a87
abanoubtalaatr <38631117+abanoubtalaatr@users.noreply.github.com>
2023-08-21 11:12:30 +0300

    fix bug in login form

--- app/Http/Controllers/User/AuthController.php
+++ app/Http/Controllers/User/AuthController.php
@@ -2,16 +2,52 @@
 
 namespace App\Http\Controllers\User;
 
+use App\Models\Setting;
+use App\Models\User;
+use App\Services\GenerateCodeService;
+use GuzzleHttp\Client;
 use Illuminate\Support\Str;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 use App\Http\Requests\EditProfileRequest;
+use Illuminate\Support\Facades\Validator;
+use function App\Helpers\sendMessageToWhatsApp;
+use function App\Helpers\sendSms;
 
 
 class AuthController extends Controller
 {
+    public function showRegisterForm()
+    {
+        return view('front.auth.register');
+    }
+
+    public function showLoginForm()
+    {
+        return view('front.auth.login');
+    }
+
+    public function showForgotPasswordForm()
+    {
+        return view('front.auth.forgot_password');
+    }
+
+
+    public function verifyForgetPasswordCode(User $user)
+    {
+        return view('front.auth.verify_forget_password_code', compact('user'));
+    }
+
+    public function verifyRegisterCode()
+    {
+        return view('livewire.front.verify-account');
+    }
+
+
+
     public function profile()
     {
+        dd('fdslfkjds');
         return view('front.auth.profile');
     }
 
